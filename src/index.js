a0e54240560514c136997c2425e92f39c9fa6277
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2019-04-02 19:19:43 +0200

    physics

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { Engine } from "@babylonjs/core/Engines/engine";
 import { Scene } from "@babylonjs/core/scene";
 import { Vector3 } from "@babylonjs/core/Maths/math";
 import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
+import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
 import { PointLight } from "@babylonjs/core/Lights/pointLight";
 import { Mesh } from "@babylonjs/core/Meshes/mesh";
 import { SpriteManager } from "@babylonjs/core/Sprites/spriteManager";
@@ -15,7 +16,7 @@ import "@babylonjs/core/Meshes/meshBuilder";
 const levelup = require("levelup");
 const leveljs = require("level-js");
 const imageCache = levelup(leveljs("images"));
-
+const relatedCache = levelup(leveljs("related"));
 function loadScript(url) {
   return new Promise((resolve, reject) => {
     const elem = document.createElement("script");
@@ -25,12 +26,45 @@ function loadScript(url) {
     document.head.appendChild(elem);
   });
 }
-
+let openplatformRequest;
+function ensureOpenplatform() {
+  if(!openplatformRequest) {
+    openplatformRequest = (async () => {
+  console.log("request open platform");
+  await loadScript("https://openplatform.dbc.dk/v3/dbc_openplatform.min.js");
+  await window.dbcOpenPlatform.connect(
+    "74b14121-aa23-4e53-b5ef-522850a13f5e",
+    "4122efb022161deef3d812a92d309043ddd5c39fcbedaf92fb0240f326889077"
+  );
+  console.log("got openplatform");
+    })()
+  }
+    return openplatformRequest;
+}
+const _related = {};
+async function related(pid) {
+  if(_related[pid]) {
+    return _related[pid];
+  }
+  let result;
+  try {
+    result =  JSON.parse(await relatedCache.get(pid));
+  } catch (e) {
+    await ensureOpenplatform();
+    result = await window.dbcOpenPlatform.recommend({
+    like: [pid],
+    limit: 100
+  });
+      await relatedCache.put(pid, JSON.stringify(result));
+  }
+  _related[pid] = result;
+  return result;
+}
 async function getCover(pid) {
   try {
     return JSON.parse(await imageCache.get(pid));
   } catch (e) {
-    console.log(e);
+    await ensureOpenplatform();
     const { coverDataUrl500 } = (await window.dbcOpenPlatform.work({
       pids: [pid],
       fields: ["coverDataUrl500"]
@@ -51,32 +85,84 @@ async function getCover(pid) {
   }
 }
 
-(async () => {
-  console.log("request open platform");
-  await loadScript("https://openplatform.dbc.dk/v3/dbc_openplatform.min.js");
-  await window.dbcOpenPlatform.connect(
-    "74b14121-aa23-4e53-b5ef-522850a13f5e",
-    "4122efb022161deef3d812a92d309043ddd5c39fcbedaf92fb0240f326889077"
-  );
-  console.log("got openplatform");
-  let recommended = await window.dbcOpenPlatform.recommend({
-    //like: ["870970-basis:29644160"],
-    //like: ["870970-basis:29841853"],
-    like: ["870970-basis:23726246"],
-    limit: 100
-  });
-  recommended = await Promise.all(
-    recommended.map(async o => ({ ...o, cover: await getCover(o.pid) }))
-  );
-  recommended = recommended.filter(o => o.cover);
-  recommended = recommended.map(o => ({ ...o, ...o.cover }));
+let pids = [];
+let covers = [];
+let recommendations = [];
+let sprites = []
+// positions, velocities, accelerations
+let ps = [], vs = [], as = [];
+async function initModel(pid) {
+  console.time('recommend');
+  let arr = (await related(pid))
+  console.timeEnd('recommend');
 
-  await createScene({ imgs: recommended });
-  engine.runRenderLoop(() => {
-    scene.render();
-  });
-})();
+  console.time('covers');
+  arr = await Promise.all(
+    arr.map(async o => ({ ...o, cover: await getCover(o.pid) }))
+  );
+  arr = arr.filter(o => o.cover);
+  console.timeEnd('covers');
 
+  pids = arr.map(o => o.pid)
+  covers = arr.map(o => o.cover);
+
+  console.time('recommend2');
+  recommendations = await Promise.all(pids.map(async pid => (await related(pid))))
+  console.log(recommendations);
+  console.timeEnd('recommend2');
+
+  ps = pids.map(o => new Vector3(rnd() * 10, rnd() * 10, rnd() * 10));
+  vs = pids.map(o => Vector3.Zero())
+  as = pids.map(o => Vector3.Zero())
+}
+
+function updatePos() {
+  for(let i = 0; i < pids.length; ++i) {
+    const a = Vector3.Zero();
+    const p = ps[i];
+
+    const centerDistanceSq = p.lengthSquared();
+    const gravity = p.clone().normalize()
+    gravity.scaleInPlace(-0.1/centerDistanceSq );
+    a.addInPlace(gravity);
+    a.y -= 0.0002 * p.y
+
+    const dampening = vs[i].clone();
+    dampening.scaleInPlace(-0.04);
+    a.addInPlace(dampening);
+
+
+    for(let j = 0; j < pids.length; ++j) {
+      if(i !== j) {
+        // spacing
+        const d = p.subtract(ps[j])
+        const distSq = p.lengthSquared(p);
+        let push = 32 - distSq
+        if(push > 0) {
+          push = push * push * push * push;
+          d.normalize()
+          d.scaleInPlace(push * 0.001)
+          a.addInPlace(d)
+        }
+      }
+    }
+    as[i] = a;
+  }
+
+  for(let i = 0; i < pids.length; ++i) {
+    vs[i] = vs[i].addInPlace(as[i])
+    ps[i] = ps[i].addInPlace(vs[i])
+  }
+  setTimeout(updatePos, 0);
+}
+
+async function main() {
+  await initModel("870970-basis:23726246");
+
+  console.time('scene');
+  await createScene();
+  console.timeEnd('scene');
+}
 function rnd() {
   return (
     Math.random() +
@@ -87,45 +173,49 @@ function rnd() {
     Math.random()
   );
 }
-
+async function createScene() {
 const canvas = document.getElementById("renderCanvas");
 const engine = new Engine(canvas, true);
 var scene = new Scene(engine);
+  //var camera = new ArcRotateCamera("Camera", 1, 1, 15, new Vector3(0, 0, 0), scene);
+  var camera = new FreeCamera("camera1", new Vector3(0, 2, 0), scene);
+  camera.attachControl(canvas, true);
+  //  camera.setTarget(Vector3.Zero());
+  try {
 var vrHelper = scene.createDefaultVRExperience({
   createDeviceOrientationCamera: false,
   useMultiview: true
 });
+} catch(e) {
+  console.log(e)
+}
 
-async function createScene({ imgs }) {
-  // Create camera and light
+
   var light = new PointLight("Point", new Vector3(5, 10, 5), scene);
-  var camera = new FreeCamera("camera1", new Vector3(0, 2, 0), scene);
-  //  camera.setTarget(Vector3.Zero());
-  camera.attachControl(canvas, true);
 
-  for (let i = 0; i < imgs.length; ++i) {
-    const width = imgs[i].width;
-    const height = imgs[i].height;
+  for (let i = 0; i < covers.length; ++i) {
+    const width = covers[i].width;
+    const height = covers[i].height;
     const size = Math.sqrt(width * width + height * height) / 1.5;
     const spriteManager = new SpriteManager(
       "cover" + i,
-      imgs[i].src,
+      covers[i].src,
       1,
       { width, height },
       scene
     );
     const sprite = new Sprite("cover", spriteManager);
-    let x, y, z;
-    do {
-      x = rnd() * 5;
-      y = rnd() * 1 + 2;
-      z = rnd() * 5;
-    } while (x * x + z * z < 2);
-    console.log(x, y, z);
-    sprite.position = { x, y, z };
     sprite.width = width / size;
     sprite.height = height / size;
+    sprites.push(sprite);
   }
 
-  return scene;
+  engine.runRenderLoop(() => {
+    for(let i = 0; i < sprites.length; ++i) {
+      sprites[i].position = ps[i].add({x:0, y:1,z:0});
+    }
+    updatePos()
+    scene.render();
+  });
 }
+main();
