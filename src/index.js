89b8e824651dc6343f3f0996d17e0e5a447aa4c6
Rasmus Erik Voel Jensen <github-rasmuserik@solsort.dk>
2019-04-03 09:46:20 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -28,36 +28,38 @@ function loadScript(url) {
 }
 let openplatformRequest;
 function ensureOpenplatform() {
-  if(!openplatformRequest) {
+  if (!openplatformRequest) {
     openplatformRequest = (async () => {
-  console.log("request open platform");
-  await loadScript("https://openplatform.dbc.dk/v3/dbc_openplatform.min.js");
-  await window.dbcOpenPlatform.connect(
-    "74b14121-aa23-4e53-b5ef-522850a13f5e",
-    "4122efb022161deef3d812a92d309043ddd5c39fcbedaf92fb0240f326889077"
-  );
-  console.log("got openplatform");
-    })()
+      console.log("request open platform");
+      await loadScript(
+        "https://openplatform.dbc.dk/v3/dbc_openplatform.min.js"
+      );
+      await window.dbcOpenPlatform.connect(
+        "74b14121-aa23-4e53-b5ef-522850a13f5e",
+        "4122efb022161deef3d812a92d309043ddd5c39fcbedaf92fb0240f326889077"
+      );
+      console.log("got openplatform");
+    })();
   }
-    return openplatformRequest;
+  return openplatformRequest;
 }
 const _related = {};
 let relCount = 0;
 async function related(pid) {
-  if(_related[pid]) {
+  if (_related[pid]) {
     return _related[pid];
   }
   let result;
   try {
-    result =  JSON.parse(await relatedCache.get(pid));
+    result = JSON.parse(await relatedCache.get(pid));
   } catch (e) {
     await ensureOpenplatform();
     result = await window.dbcOpenPlatform.recommend({
-    like: [pid],
-    limit: 100
-  });
-      await relatedCache.put(pid, JSON.stringify(result));
-	  console.log(++relCount, 'related', pid);
+      like: [pid],
+      limit: 100
+    });
+    await relatedCache.put(pid, JSON.stringify(result));
+    console.log(++relCount, "related", pid);
   }
   _related[pid] = result;
   return result;
@@ -91,34 +93,38 @@ let pids = [];
 let covers = [];
 let recommendations = [];
 let recMat = [];
-let sprites = []
+let sprites = [];
 // positions, velocities, accelerations
-let ps = [], vs = [], as = [];
+let ps = [],
+  vs = [],
+  as = [];
 async function initModel(pid) {
-  console.time('recommend');
-  let arr = (await related(pid))
-  console.timeEnd('recommend');
+  console.time("recommend");
+  let arr = await related(pid);
+  console.timeEnd("recommend");
 
-  console.time('covers');
+  console.time("covers");
   arr = await Promise.all(
     arr.map(async o => ({ ...o, cover: await getCover(o.pid) }))
   );
   arr = arr.filter(o => o.cover);
-  console.timeEnd('covers');
+  console.timeEnd("covers");
 
-  pids = arr.map(o => o.pid)
+  pids = arr.map(o => o.pid);
   covers = arr.map(o => o.cover);
 
-  console.time('recommend2');
-  recommendations = await Promise.all(pids.map(async pid => (await related(pid))))
+  console.time("recommend2");
+  recommendations = await Promise.all(
+    pids.map(async pid => await related(pid))
+  );
   console.log(recommendations);
-  console.timeEnd('recommend2');
+  console.timeEnd("recommend2");
 
   recMat = pids.map(() => new Float64Array(pids.length));
-  for(let i = 0; i < recommendations.length; ++i) {
-    for(const o of recommendations[i]) {
-      const pos = pids.indexOf(o.pid) 
-      if(pos !== -1) {
+  for (let i = 0; i < recommendations.length; ++i) {
+    for (const o of recommendations[i]) {
+      const pos = pids.indexOf(o.pid);
+      if (pos !== -1) {
         recMat[i][pos] += o.val;
         recMat[pos][i] += o.val;
       }
@@ -127,68 +133,67 @@ async function initModel(pid) {
   console.log(recMat);
 
   ps = pids.map(() => new Vector3(rnd() * 10, rnd() * 10, rnd() * 10));
-  vs = pids.map(() => Vector3.Zero())
-  as = pids.map(() => Vector3.Zero())
+  vs = pids.map(() => Vector3.Zero());
+  as = pids.map(() => Vector3.Zero());
 }
 
 function updatePos() {
-  for(let i = 0; i < pids.length; ++i) {
+  for (let i = 0; i < pids.length; ++i) {
     const a = Vector3.Zero();
     const p = ps[i];
 
     const centerDistanceSq = p.lengthSquared();
-    const gravity = p.clone().normalize()
-    gravity.scaleInPlace(-1/centerDistanceSq );
+    const gravity = p.clone().normalize();
+    gravity.scaleInPlace(-1 / centerDistanceSq);
     a.addInPlace(gravity);
-    a.y -= Math.max(0, 0.01 * p.y -1)
+    a.y -= Math.max(0, 0.01 * p.y - 1);
 
     const dampening = vs[i].clone();
     dampening.scaleInPlace(-0.6);
     a.addInPlace(dampening);
 
-
-    for(let j = 0; j < pids.length; ++j) {
-      if(i !== j) {
+    for (let j = 0; j < pids.length; ++j) {
+      if (i !== j) {
         // spacing
-        let d = p.subtract(ps[j])
+        let d = p.subtract(ps[j]);
         const distSq = p.lengthSquared(p);
-        let push = 4 - distSq
-        if(push > 0) {
+        let push = 4 - distSq;
+        if (push > 0) {
           push = push * push * push * push;
-          d.normalize()
-          d.scaleInPlace(push * 0.001)
-          a.addInPlace(d)
+          d.normalize();
+          d.scaleInPlace(push * 0.001);
+          a.addInPlace(d);
         }
 
         // attraction
         d = ps[j].subtract(p);
-        d.normalize()
+        d.normalize();
         d.scaleInPlace(recMat[i][j] * 0.001);
-        a.addInPlace(d)
+        a.addInPlace(d);
       }
     }
     as[i] = a;
   }
 
-  for(let i = 0; i < pids.length; ++i) {
-    vs[i] = vs[i].addInPlace(as[i])
-    ps[i] = ps[i].addInPlace(vs[i])
+  for (let i = 0; i < pids.length; ++i) {
+    vs[i] = vs[i].addInPlace(as[i]);
+    ps[i] = ps[i].addInPlace(vs[i]);
   }
   setTimeout(updatePos, 0);
 }
 
 async function main() {
-	let pid;
-	pid = "870970-basis:23726246" // allende
-pid = 	 "870970-basis:29644160" // borneboger
-	 pid = "870970-basis:22331892" // hesse
-	 pid ="870970-basis:29841853"  // action movies
+  let pid;
+  pid = "870970-basis:29644160"; // borneboger
+  pid = "870970-basis:22331892"; // hesse
+  pid = "870970-basis:29841853"; // action movies
+  pid = "870970-basis:23726246"; // allende
 
   await initModel(pid);
 
-  console.time('scene');
+  console.time("scene");
   await createScene();
-  console.timeEnd('scene');
+  console.timeEnd("scene");
 }
 function rnd() {
   return (
@@ -201,22 +206,21 @@ function rnd() {
   );
 }
 async function createScene() {
-const canvas = document.getElementById("renderCanvas");
-const engine = new Engine(canvas, true);
-var scene = new Scene(engine);
+  const canvas = document.getElementById("renderCanvas");
+  const engine = new Engine(canvas, true);
+  var scene = new Scene(engine);
   //var camera = new ArcRotateCamera("Camera", 1, 1, 15, new Vector3(0, 0, 0), scene);
   var camera = new FreeCamera("camera1", new Vector3(0, 2, 0), scene);
   camera.attachControl(canvas, true);
   //  camera.setTarget(Vector3.Zero());
   try {
-var vrHelper = scene.createDefaultVRExperience({
-  createDeviceOrientationCamera: false,
-  useMultiview: true
-});
-} catch(e) {
-  console.log(e)
-}
-
+    var vrHelper = scene.createDefaultVRExperience({
+      createDeviceOrientationCamera: false,
+      useMultiview: true
+    });
+  } catch (e) {
+    console.log(e);
+  }
 
   var light = new PointLight("Point", new Vector3(5, 10, 5), scene);
 
@@ -238,11 +242,11 @@ var vrHelper = scene.createDefaultVRExperience({
   }
 
   engine.runRenderLoop(() => {
-    for(let i = 0; i < sprites.length; ++i) {
-	    const p = ps[i];
-      sprites[i].position = {x: p.x, y: p.y / 2 + 1.5, z: p.z}
+    for (let i = 0; i < sprites.length; ++i) {
+      const p = ps[i];
+      sprites[i].position = { x: p.x, y: p.y / 2 + 1.5, z: p.z };
     }
-    updatePos()
+    updatePos();
     scene.render();
   });
 }
